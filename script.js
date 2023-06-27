fbe29308cf255b749b99bed0fb45aa5a4b2732e8
Saajan Singh Rana <123057148+saajanrana@users.noreply.github.com>
2023-06-27 03:17:37 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your code here
+const api = "https://randomuser.me/api/";
+
+const fullName = document.querySelector(".name");
+const image = document.querySelector(".image");
+const age = document.querySelector(".age");
+const phone = document.querySelector(".phone");
+const email = document.querySelector(".email");
+const bigBtn = document.querySelector(".big-btn");
+const output = document.querySelector(".additional-info");
+
+let emailData, ageData, phoneData;
+
+const fetchData = async () => {
+  const response = await fetch(api);
+  const data = await response.json();
+  const result = data.results[0];
+  image.innerHTML = `<img src=${result.picture.large} />`;
+  fullName.innerHTML = `<h3>${result.name.first} ${result.name.last}</h3>`;
+  emailData = result.email;
+  ageData = result.dob.age;
+  phoneData = result.cell;
+};
+
+window.onload = () => {
+  fetchData();
+};
+
+age.addEventListener("click", () => {
+  output.innerText = ageData;
+});
+
+phone.addEventListener("click", () => {
+  output.innerText = phoneData;
+});
+
+email.addEventListener("click", () => {
+  output.innerText = emailData;
+});
+
+bigBtn.addEventListener("click", () => {
+  output.innerText = "";
+  fetchData();
+});
